411b98924b7c6570ac16633ecc719020519f2a05
mohammad-fattah <m.p.fattahpour@gmail.com>
2020-08-16 01:50:35 +0430

    edit bash file

--- server_download.sh
+++ server_download.sh
@@ -1,2 +1,5 @@
-sudo wget -c https://loh.istgahesalavati.ir/report.gz.tar && sudo tar -xzf  report.gz.tar -C /tmp/final_project/
-
+cd /tmp 
+rm -rf final_project
+rm -rf report.gz.tar
+mkdir final_project
+wget -c https://loh.istgahesalavati.ir/report.gz.tar && tar -xzf  report.gz.tar -C /tmp/final_project/
